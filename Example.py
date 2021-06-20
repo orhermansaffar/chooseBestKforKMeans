1ce4647bf83558d63771f461e3679f14c1788ff1
orhermansaffar <59613363+orhermansaffar@users.noreply.github.com>
2021-06-20 18:11:04 +0300

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,20 +1,21 @@
-# imports
-import pandas as pd
-import numpy as np
-from sklearn.cluster import KMeans
-from joblib import Parallel, delayed
+# How to use chooseBestKforKMeans
+
 # choose features
 data_for_clustering = data[features_for_clustering].copy()
 data_for_clustering.fillna(0,inplace=True)
+
 # create data matrix
 data_matrix = np.matrix(data_for_clustering).astype(float)
 # scale the data
 from sklearn.preprocessing import MinMaxScaler
 mms = MinMaxScaler()
 scaled_data = mms.fit_transform(data_matrix)
+
 # choose k range
 k_range=range(2,20)
+# compute adjusted intertia
 best_k, results = chooseBestKforKMeansParallel(scaled_data, k_range)
+
 # plot the results
 plt.figure(figsize=(7,4))
 plt.plot(results,'o')
