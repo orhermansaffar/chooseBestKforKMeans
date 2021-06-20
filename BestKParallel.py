2c77a7aaf580c73117be48f19c2859d82df9d7f5
orhermansaffar <59613363+orhermansaffar@users.noreply.github.com>
2021-06-20 18:17:11 +0300

    Update BestKParallel.py

--- BestKParallel.py
+++ BestKParallel.py
@@ -5,6 +5,21 @@ from sklearn.cluster import KMeans
 from joblib import Parallel, delayed
 
 def kMeansRes(scaled_data, k, alpha_k=0.02):
+    '''
+    Parameters 
+    ----------
+    scaled_data: matrix 
+        scaled data. rows are samples and columns are features for clustering
+    k: int
+        current k for applying KMeans
+    alpha_k: float
+        manually tuned factor that gives penalty to the number of clusters
+    Returns 
+    -------
+    scaled_inertia: float
+        scaled inertia value for current k           
+    '''
+    
     inertia_o = np.square((scaled_data - scaled_data.mean(axis=0))).sum()
     # fit k-means
     kmeans = KMeans(n_clusters=k, random_state=0).fit(scaled_data)
@@ -12,6 +27,22 @@ def kMeansRes(scaled_data, k, alpha_k=0.02):
     return scaled_inertia
 
 def chooseBestKforKMeansParallel(scaled_data, k_range):
+    '''
+    Parameters 
+    ----------
+    scaled_data: matrix 
+        scaled data. rows are samples and columns are features for clustering
+    k_range: list of integers
+        k range for applying KMeans
+    Returns 
+    -------
+    best_k: int
+        chosen value of k out of the given k range.
+        chosen k is k with the minimum scaled inertia value.
+    results: pandas DataFrame
+        adjusted inertia value for each k in k_range
+    '''
+    
     ans = Parallel(n_jobs=-1,verbose=10)(delayed(kMeansRes)(scaled_data, k) for k in k_range)
     ans = list(zip(k_range,ans))
     results = pd.DataFrame(ans, columns = ['k','Scaled Inertia']).set_index('k')
