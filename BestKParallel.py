4d604897b44198207919bf9cf2ca150997b77fed
orhermansaffar <59613363+orhermansaffar@users.noreply.github.com>
2021-06-20 18:00:14 +0300

    Create BestKParallel.py

---
+++ BestKParallel.py
@@ -0,0 +1,19 @@
+# imports
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from joblib import Parallel, delayed
+
+def kMeansRes(scaled_data, k, alpha_k=0.01):
+    inertia_o = np.square((scaled_data - scaled_data.mean(axis=0))).sum()
+    # fit k-means
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(scaled_data)
+    scaled_inertia = kmeans.inertia_
+    return scaled_inertia
+
+def chooseBestKforKMeansParallel(scaled_data, k_range):
+    ans = Parallel(n_jobs=-1,verbose=10)(delayed(kMeansRes)(scaled_data, k) for k in k_range)
+    ans = list(zip(k_range,ans))
+    results = pd.DataFrame(ans, columns = ['k','Scaled Inertia']).set_index('k')
+    best_k = results.idxmin()[0]
+    return best_k, results
